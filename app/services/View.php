bbab3787954a5bf460d59fcb379535b7dd05cdd1
oleg <oleg@mordvincev.ru>
2024-10-16 23:29:34 +0500

    Обновление вьювера

--- app/services/View.php
+++ app/services/View.php
@@ -6,8 +6,25 @@ namespace services;
 
 final class View
 {
+    private const XML_TYPE = 'xml';
+    private const HTML_TYPE = 'html';
+    //private const JSON_TYPE = 'json';
+
+    private const ALLOW_TYPES = [self::XML_TYPE, self::HTML_TYPE/*, self::JSON_TYPE*/];
+
+    private string $type;
+
+    public function __construct(string $type = self::XML_TYPE)
+    {
+        if (! in_array($type, self::ALLOW_TYPES, true)) {
+            throw new \RuntimeException("Неизвестный тип для вывода: $type");
+        }
+
+        $this->type = $type;
+    }
+
     public function __call(string $view, array $args)
     {
-        echo strtr(require __DIR__ . "/../views/xml/{$view}.php", $args[0] ?? []);
+        echo strtr(require __DIR__ . "/../views/{$this->type}/{$view}.php", $args[0] ?? []);
     }
 }
